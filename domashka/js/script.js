e2056f939f55e441c5c1ca11fdfd6e7c8157dc60
Garrus777rus <saren500@gmail.com>
2015-11-23 20:40:04 +0400

    Domashka 5-6, 100% ready!

--- domashka/js/script.js
+++ domashka/js/script.js
@@ -5,3 +5,14 @@
 var x=parseInt(window.prompt('Введите X'));
 var y=parseInt(window.prompt('Введите Y'));
 var z=parseInt(window.prompt('Введите Z'));
+if(x<y && x<z) var samoeMalenkoe = x;
+if(y<x && y<z) var samoeMalenkoe = y;
+if(z<x && z<y) var samoeMalenkoe = z;
+if(x>y && x>z) var samoeBolshoe = x;
+if(y>x && y>z) var samoeBolshoe = y;
+if(z>x && z>y) var samoeBolshoe = z;
+if(x>y && x<z || x<y && x>z) var srednee = x;
+if(y>x && y<z || y<x && y>z) var srednee = y;
+if(z>x && z<y || z<x && z>y) var srednee = z;
+
+console.log(samoeMalenkoe + ' ' + srednee + ' ' +samoeBolshoe);
